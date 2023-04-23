147ccfeaa8f7cfc74616dc24ac16a310ef56a30e
alifsakib <alifsakib@gmail.com>
2023-04-23 19:24:54 +0600

    applyMiddleware crated

--- middlewares.js
+++ middlewares.js
@@ -1,43 +1,17 @@
-const fetch = require("node-fetch");
+const delayActionsMiddleware = (store) => (next) => (action) => {
+  if (action.type === "todos/todoAdded") {
+    console.log("I am delaying you!");
 
-const delayActionMiddleware = (store) => (next) => (action) => {
-    if (action.type === "todos/todoAdded") {
-        console.log("I am delaying you!");
+    setTimeout(() => {
+      next(action);
+    }, 2000);
 
-        setTimeout(() => {
-            next(action);
-        }, 2000);
+    return;
+  }
 
-        return;
-    }
-
-    return next(action);
-};
-
-const fetchTodosMiddleware = (store) => (next) => async (action) => {
-    if (action.type === "todos/fetchTodos") {
-        const response = await fetch(
-            "https://jsonplaceholder.typicode.com/todos?_limit=5"
-        );
-
-        const todos = await response.json();
-
-        store.dispatch({
-            type: "todos/todoLoaded",
-            payload: todos,
-        });
-
-        console.log(
-            `Number of updated todos: ${store.getState().todos.length}`
-        );
-
-        return;
-    }
-
-    return next(action);
+  return next(action);
 };
 
 module.exports = {
-    delayActionMiddleware,
-    fetchTodosMiddleware,
+  delayActionsMiddleware,
 };
