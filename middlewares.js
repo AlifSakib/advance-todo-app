d0fd661282ecf42801e3dada730d832b3acd98e4
alifsakib <alifsakib@gmail.com>
2023-04-23 19:46:51 +0600

    fetched todo from json placeholder

--- middlewares.js
+++ middlewares.js
@@ -1,3 +1,5 @@
+const fetch = require("node-fetch");
+
 const delayActionsMiddleware = (store) => (next) => (action) => {
   if (action.type === "todos/todoAdded") {
     console.log("I am delaying you!");
@@ -5,6 +7,23 @@ const delayActionsMiddleware = (store) => (next) => (action) => {
     setTimeout(() => {
       next(action);
     }, 2000);
+
+    return;
+  }
+
+  return next(action);
+};
+
+const fetchTodoMiddleware = (store) => (next) => async (action) => {
+  if (action.type === "todos/fetchTodos") {
+    const response = await fetch(
+      "https://jsonplaceholder.typicode.com/todos?_limit=5"
+    );
+    const todos = (aawait = response.json());
+    store.dispatch({
+      type: "todos/todoLoaded",
+      payload: todos,
+    });
 
     return;
   }
